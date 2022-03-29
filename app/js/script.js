ed64626d48085ceb8bffca1743307e7676a89376
Hum Bruno <hsbruno1@gmail.com>
2022-03-29 22:49:42 +0100

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector("button");
+const navMenu = document.querySelector(".navMenu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+
+function buildStyles() {
+  return src("app/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["app/scss/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
