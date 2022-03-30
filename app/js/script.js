b0cbab29d45a1b377bffa473496a0dc246249560
Hum Bruno <hsbruno1@gmail.com>
2022-03-30 21:55:37 +0100

    hero styles and mobile nav

--- app/js/script.js
+++ app/js/script.js
@@ -3,4 +3,5 @@ const navMenu = document.querySelector(".navMenu");
 
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
+  menuToggle.classList.toggle("open");
 });
