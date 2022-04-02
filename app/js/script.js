6054a504f617aaa25e6d24520ac39adf7c3f6133
Hum Bruno <hsbruno1@gmail.com>
2022-04-02 21:22:43 +0100

    final desktop styles and projects description

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,15 @@
 const menuToggle = document.querySelector("button");
 const navMenu = document.querySelector(".navMenu");
+const navLinks = document.querySelectorAll(".navMenu a");
 
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   menuToggle.classList.toggle("open");
 });
+
+for (const navLink of navLinks) {
+  navLink.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    menuToggle.classList.remove("open");
+  });
+}
